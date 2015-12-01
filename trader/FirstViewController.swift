25771ae19d3fa5a13524a92feaff110b4b1b4498
Xi Zhang <xizhang@bos-imac.local>
2015-11-30 18:30:35 -0800

    Initial Commit

---
+++ trader/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  trader
+//
+//  Created by Xi Zhang on 11/30/15.
+//  Copyright © 2015 minor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
